8b2176e51ee18e3d972f92e13217ac65cc846644
swiggins83 <stevenwiggins83@gmail.com>
2015-09-13 18:35:07 -0400

    Set up activities with annotations and dummy views

---
+++ app/src/main/java/edu/oakland/festinfo/activities/HomePageActivity.java
@@ -0,0 +1,11 @@
+package edu.oakland.festinfo.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+import edu.oakland.festinfo.R;
+
+@EActivity(R.layout.activity_main)
+public class HomePageActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/edu/oakland/festinfo/activities/UserProfileActivity.java
@@ -0,0 +1,11 @@
+package edu.oakland.festinfo.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+import edu.oakland.festinfo.R;
+
+@EActivity(R.layout.activity_user_profile)
+public class UserProfileActivity extends AppCompatActivity {
+}

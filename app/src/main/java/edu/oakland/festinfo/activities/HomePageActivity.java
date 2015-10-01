f48347c6d160d953bb986c9056c4121cc0eaea6e
Devin <dpkelly@oakland.edu>
2015-09-30 20:02:56 -0400

    gui for appIntro created

--- app/src/main/java/edu/oakland/festinfo/activities/HomePageActivity.java
+++ app/src/main/java/edu/oakland/festinfo/activities/HomePageActivity.java
@@ -1,26 +1,28 @@
 package edu.oakland.festinfo.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-
-
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.Toast;
+import android.content.Intent;
+
 
 import com.parse.ParseUser;
 
@@ -31,159 +33,170 @@ import org.androidannotations.annotations.ViewById;
 
 import edu.oakland.festinfo.R;
 
-@EActivity(R.layout.activity_main)
+//@EActivity(R.layout.activity_main)
 public class HomePageActivity extends AppCompatActivity {
 
-    private String[] mNavigationDrawerItemTitles;
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-
-    private CharSequence mDrawerTitle;
-    private CharSequence mTitle;
+    //Defining Variables
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
 
 
+    //@ViewById(R.id.current_user)
+    //TextView currentUserTextView;
 
-    @ViewById(R.id.current_user)
-    TextView currentUserTextView;
+    //@AfterViews
+    public void onCreate(Bundle savedInstanceState) {
 
-    @AfterViews
-    public void init() {
-        ParseUser parseUser = new ParseUser();
-        currentUserTextView.setText(parseUser.getCurrentUser().getUsername());
+        super.onCreate(savedInstanceState);
+        //ParseUser parseUser = new ParseUser();
+        //currentUserTextView.setText(parseUser.getCurrentUser().getUsername());
 
-        mNavigationDrawerItemTitles= getResources().getStringArray(R.array.navigation_drawer_items_array);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-
-        ObjectDrawerItem[] drawerItem = new ObjectDrawerItem[3];
-
-        drawerItem[0] = new ObjectDrawerItem(R.mipmap.searchicon, "Create");
-        drawerItem[1] = new ObjectDrawerItem(R.mipmap.ic_launcher, "Read");
-        drawerItem[2] = new ObjectDrawerItem(R.mipmap.ic_launcher, "Help");
-
-        DrawerItemCustomAdapter adapter = new DrawerItemCustomAdapter(this, R.layout.listview_item_row, drawerItem);
-
-        mDrawerList.setAdapter(adapter);
-
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        setContentView(R.layout.activity_main);
 
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Initializing NavigationView
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
 
 
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if (menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
 
-        ActionBarDrawerToggle mDrawerToggle;
+                //Closing drawer on item click
+                drawerLayout.closeDrawers();
 
-        mTitle = mDrawerTitle = getTitle();
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
 
 
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.user_profile:
+                        Toast.makeText(getApplicationContext(), "User Profile Selected", Toast.LENGTH_SHORT).show();
+                        UserProfileActivity fragment = new UserProfileActivity();
+                        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.frame, fragment);
+                        fragmentTransaction.commit();
+                        return true;
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerToggle = new ActionBarDrawerToggle(
-                this,
-                mDrawerLayout,
-                R.string.drawer_open,
-                R.string.drawer_close
-        ) {
+                    // For rest of the options we just show a toast on click
 
-            /** Called when a drawer has settled in a completely closed state. */
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                getActionBar().setTitle(mTitle);
+                    case R.id.search:
+                        Toast.makeText(getApplicationContext(), "Search Selected", Toast.LENGTH_SHORT).show();
+                        SearchPageActivity fragment2 = new SearchPageActivity();
+                        android.support.v4.app.FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction2.replace(R.id.frame, fragment2);
+                        fragmentTransaction2.commit();
+                        return true;
+
+                    case R.id.directions:
+                        Toast.makeText(getApplicationContext(), "Directions Selected", Toast.LENGTH_SHORT).show();
+                        DirectionsPageActivity fragment3 = new DirectionsPageActivity();
+                        android.support.v4.app.FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction3.replace(R.id.frame, fragment3);
+                        fragmentTransaction3.commit();
+                        return true;
+
+                    case R.id.weather:
+                        Toast.makeText(getApplicationContext(), "Weather Selected", Toast.LENGTH_SHORT).show();
+                        WeatherPageActivity fragment4 = new WeatherPageActivity();
+                        android.support.v4.app.FragmentTransaction fragmentTransaction4 = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction4.replace(R.id.frame, fragment4);
+                        fragmentTransaction4.commit();
+                        return true;
+
+                    case R.id.tickets:
+                        Toast.makeText(getApplicationContext(), "Tickets Selected", Toast.LENGTH_SHORT).show();
+                        TicketsPageActivity fragment5 = new TicketsPageActivity();
+                        android.support.v4.app.FragmentTransaction fragmentTransaction5 = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction5.replace(R.id.frame, fragment5);
+                        fragmentTransaction5.commit();
+                        return true;
+
+                    case R.id.notifications:
+                        Toast.makeText(getApplicationContext(), "Notifications Selected", Toast.LENGTH_SHORT).show();
+                        NotificationsPageActivity fragment6 = new NotificationsPageActivity();
+                        android.support.v4.app.FragmentTransaction fragmentTransaction6 = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction6.replace(R.id.frame, fragment6);
+                        fragmentTransaction6.commit();
+                        return true;
+
+                    case R.id.logout:
+                        Toast.makeText(getApplicationContext(), "Logout Selected", Toast.LENGTH_SHORT).show();
+                        LogoutPage fragment7 = new LogoutPage();
+                        android.support.v4.app.FragmentTransaction fragmentTransaction7 = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction7.replace(R.id.frame, fragment7);
+                        fragmentTransaction7.commit();
+                        return true;
+                    default:
+                        Toast.makeText(getApplicationContext(), "Somethings Wrong", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                }
+            }
+        });
+
+        // Initializing Drawer Layout and ActionBarToggle
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
             }
 
-            /** Called when a drawer has settled in a completely open state. */
+            @Override
             public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+
                 super.onDrawerOpened(drawerView);
-                getActionBar().setTitle(mDrawerTitle);
             }
         };
 
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        //Setting the actionbarToggle to drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
 
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setHomeButtonEnabled(true);
-
+        //calling sync state is necessay or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
 
 
     }
 
-
-
-    @Click(R.id.sign_in)
-    public void signIn() {
-        LoginActivity_
-                .intent(this)
-                .start();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
-
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
-        }
-
-    }
-
-    private void selectItem(int position) {
-
-        Fragment fragment = null;
-
-        switch (position) {
-            case 0:
-                fragment = new CreateFragment();
-                break;
-            case 1:
-                //fragment = new ReadFragment();
-                break;
-            case 2:
-                //fragment = new HelpFragment();
-                break;
-
-            default:
-                break;
-        }
-
-        if (fragment != null) {
-            FragmentManager fragmentManager = getFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
-
-            mDrawerList.setItemChecked(position, true);
-            mDrawerList.setSelection(position);
-            setTitle(mNavigationDrawerItemTitles[position]);
-            mDrawerLayout.closeDrawer(mDrawerList);
-
-        } else {
-            Log.e("MainActivity", "Error in creating fragment");
-        }
-    }
-
-    /*@Override
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
-*/
-    @Override
-    public void setTitle(CharSequence title) {
-        mTitle = title;
-        getActionBar().setTitle(mTitle);
-    }
-/*
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        mDrawerToggle.syncState();
-    }
-*/
-
-
 
 
 }
+
+
 
--- app/src/main/java/edu/oakland/festinfo/utils/ParseUtil.java
+++ app/src/main/java/edu/oakland/festinfo/utils/ParseUtil.java
@@ -1,55 +1,86 @@
 package edu.oakland.festinfo.utils;
 
-import android.app.Activity;
-import android.content.Context;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
-import android.view.View;
 
-import com.parse.LogInCallback;
-import com.parse.ParseException;
-import com.parse.ParseUser;
-import com.parse.SignUpCallback;
+         import android.app.Activity;
+         import android.content.Context;
+         import android.support.design.widget.Snackbar;
+         import android.util.Log;
+         import android.view.View;
 
-import edu.oakland.festinfo.R;
+
+         import com.parse.LogInCallback;
+         import com.parse.ParseException;
+         import com.parse.ParseUser;
+         import com.parse.SignUpCallback;
+
+
+         import org.androidannotations.annotations.Background;
+         import org.androidannotations.annotations.EBean;
+
+
+         import edu.oakland.festinfo.R;
+         import edu.oakland.festinfo.activities.HomePageActivity_;
+
+
+         @EBean(scope = EBean.Scope.Singleton)
+
 
 public class ParseUtil {
 
-    public static void signUp(final Context context, String email, String username, String password) {
-        ParseUser user = new ParseUser();
-        user.setEmail(email);
-        user.setUsername(username);
-        user.setPassword(password);
-        user.signUpInBackground(new SignUpCallback() {
+
+
+    @Background
+
+
+    public void signUp(final Context context, String email, String username, String password) {
+         ParseUser currentUser = ParseUser.getCurrentUser();
+         if (currentUser != null) {
+             ParseUser.logOut();
+
+        }
+         ParseUser user = new ParseUser();
+         user.setEmail(email);
+         user.setUsername(username);
+         user.setPassword(password);
+         user.signUpInBackground(new SignUpCallback() {
             @Override
-            public void done(ParseException e) {
-                if (e == null) {
-                    // new user successfully created
-                } else {
-                    View rootView = ((Activity)context).getWindow().getDecorView().findViewById(android.R.id.content);
-                    Snackbar
-                            .make(rootView, R.string.error_occurred, Snackbar.LENGTH_LONG)
-                            .show(); // Don’t forget to show!
-                }
-            }
-        });
-    }
+                         public void done(ParseException e) {
+                                 if (e == null) {
+                                         HomePageActivity_
+                                                 .intent(context)
+                                                 .start();
+                                    } else {
+                                         View rootView = ((Activity) context).getWindow().getDecorView().findViewById(android.R.id.content);
+                                        Snackbar
+                                                 .make(rootView, R.string.error_occurred, Snackbar.LENGTH_LONG)
+                                                 .setText(StringUtils.capitalizeFirstLetter(e.getLocalizedMessage()))
+                                                 .show();
+                                     }
+                            }
+                     });
+             }
 
-    public static void signIn(final Context context, String username, String password) {
-        ParseUser.logInInBackground(username, password, new LogInCallback() {
-            @Override
-            public void done(ParseUser user, ParseException e) {
-                if (e == null) {
-                    View rootView = ((Activity) context).getWindow().getDecorView().findViewById(android.R.id.content);
-                    Snackbar
-                            .make(rootView, R.string.error_occurred, Snackbar.LENGTH_LONG)
-                            .setText(user.getUsername() + " logged in!")
-                            .show(); // Don’t forget to show!
-                } else {
 
-                }
-            }
-        });
-    }
+                 @Background
+         public void signIn(final Context context, String username, String password) {
+                 ParseUser.logInInBackground(username, password, new LogInCallback() {
+                         View rootView = ((Activity) context).getWindow().getDecorView().findViewById(android.R.id.content);
+                         @Override
+                         public void done(ParseUser user, ParseException e) {
+                                 if (e == null) {
+                                         Snackbar
+                                                 .make(rootView, R.string.login_occurred, Snackbar.LENGTH_LONG)
+                                                 .setText(user.getUsername() + " logged in!")
+                                                 .show();
+                                     } else {
+                                         Snackbar
+                                                 .make(rootView, R.string.error_occurred, Snackbar.LENGTH_LONG)
+                                                 .setText(StringUtils.capitalizeFirstLetter(e.getLocalizedMessage()))
+                                                 .show();
+                                     }
+                             }
+                     });
+             }
 
-}
+
+             }

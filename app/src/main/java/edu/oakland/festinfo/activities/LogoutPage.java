a0df5a6d15311b777591ff0cbf99fcf31b1ec909
Devin <dpkelly@oakland.edu>
2015-09-30 20:00:25 -0400

    HomePageActivity

---
+++ app/src/main/java/edu/oakland/festinfo/activities/LogoutPage.java
@@ -0,0 +1,7 @@
+package edu.oakland.festinfo.activities;
+
+/**
+ * Created by Devin on 9/30/2015.
+ */
+public class LogoutPage {
+}
---
+++ app/src/main/java/edu/oakland/festinfo/activities/NotificationsPageActivity.java
@@ -0,0 +1,7 @@
+package edu.oakland.festinfo.activities;
+
+/**
+ * Created by Devin on 9/29/2015.
+ */
+public class NotificationsPageActivity {
+}

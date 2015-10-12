cdb56b09f9b0ef65c2bd37845cb6bf2035c8eec2
Devin <dpkelly@oakland.edu>
2015-10-12 17:31:34 -0400

    updated MapPageActivity to use movable markers

--- app/src/main/java/edu/oakland/festinfo/activities/MapPageActivity.java
+++ app/src/main/java/edu/oakland/festinfo/activities/MapPageActivity.java
@@ -1,11 +1,29 @@
 package edu.oakland.festinfo.activities;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.FragmentManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -13,29 +31,100 @@ import org.androidannotations.annotations.EActivity;
 import edu.oakland.festinfo.R;
 
 @EActivity(R.layout.activity_map)
-public class MapPageActivity extends BaseActivity implements OnMapReadyCallback {
+public class MapPageActivity extends BaseActivity implements OnMapClickListener, OnMapLongClickListener, OnMarkerDragListener {
+
+    final int RQS_GooglePlayServices = 1;
+    private GoogleMap myMap;
+
+    Location myLocation;
+    TextView tvLocInfo;
+
+    boolean markerClicked;
+    PolygonOptions polygonOptions;
+    Polygon polygon;
 
     @AfterViews
     void init() {
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        tvLocInfo = (TextView)findViewById(R.id.locinfo);
+
+        FragmentManager myFragmentManager = getFragmentManager();
+        MapFragment myMapFragment = (MapFragment)myFragmentManager.findFragmentById(R.id.map);
+        myMap = myMapFragment.getMap();
+
+        myMap.setMyLocationEnabled(true);
+
+        myMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        myMap.setOnMapClickListener(this);
+        myMap.setOnMapLongClickListener(this);
+        myMap.setOnMarkerDragListener(this);
+
+        markerClicked = false;
     }
 
     @Override
-    public void onMapReady(GoogleMap mMap) {
-        LatLng sydney = new LatLng(-33.867, 151.206);
-        double lat = -33.867;
-        double lon = 151.206;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_map, menu);
+        return true;
 
-        mMap.setMyLocationEnabled(true);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+    }
 
-        mMap.addMarker(new MarkerOptions()
-                .title("Sydney")
-                .snippet("The most populous city in Australia")
-                .position(sydney));
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_legalnotices:
+                String LicenseInfo = GooglePlayServicesUtil.getOpenSourceSoftwareLicenseInfo(getApplicationContext());
+                AlertDialog.Builder LicenseDialog = new AlertDialog.Builder(MapPageActivity.this);
+                LicenseDialog.setTitle("Legal Notices");
+                LicenseDialog.setMessage(LicenseInfo);
+                LicenseDialog.show();
 
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+
+        if (resultCode == ConnectionResult.SUCCESS){
+            Toast.makeText(getApplicationContext(), "isGooglePlayServicesAvailable SUCESS", Toast.LENGTH_LONG).show();
+        }else{
+            GooglePlayServicesUtil.getErrorDialog(resultCode, this, RQS_GooglePlayServices);
+        }
+    }
+
+    @Override
+    public void onMapLongClick(LatLng point){
+        tvLocInfo.setText("New marker added@" + point.toString());
+        myMap.addMarker(new MarkerOptions()
+                .position(point)
+                .draggable(true));
+
+        markerClicked = false;
+    }
+
+    @Override
+    public void onMapClick(LatLng point) {
+        tvLocInfo.setText(point.toString());
+        myMap.animateCamera(CameraUpdateFactory.newLatLng(point));
+
+        markerClicked = false;
+    }
+
+    public void onMarkerDrag(Marker marker) {
+        tvLocInfo.setText("Marker " + marker.getId() + "Drag@" + marker.getPosition());
+    }
+
+    public void onMarkerDragEnd(Marker marker) {
+        tvLocInfo.setText("Marker " + marker.getId() + " DragEnd");
+    }
+
+    public void onMarkerDragStart(Marker marker) {
+        tvLocInfo.setText("Marker " + marker.getId() + " DragStart");
     }
 
 

cb701e1bed321dde5de58deb5a9479908c61e9d0
swiggins83 <stevenwiggins83@gmail.com>
2015-10-12 21:19:44 -0400

    Map focuses on OU / EC parking lot boundaried
    Use new API key

--- app/src/main/java/edu/oakland/festinfo/activities/MapPageActivity.java
+++ app/src/main/java/edu/oakland/festinfo/activities/MapPageActivity.java
@@ -1,32 +1,30 @@
 package edu.oakland.festinfo.activities;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
-import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polygon;
-import com.google.android.gms.maps.model.PolygonOptions;
-
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.FragmentManager;
-import android.location.Location;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 import edu.oakland.festinfo.R;
 
@@ -34,39 +32,54 @@ import edu.oakland.festinfo.R;
 public class MapPageActivity extends BaseActivity implements OnMapClickListener, OnMapLongClickListener, OnMarkerDragListener {
 
     final int RQS_GooglePlayServices = 1;
-    private GoogleMap myMap;
+    private GoogleMap map;
+    MapFragment mapFragment;
 
-    Location myLocation;
+    @ViewById(R.id.locinfo)
     TextView tvLocInfo;
-
-    boolean markerClicked;
-    PolygonOptions polygonOptions;
-    Polygon polygon;
 
     @AfterViews
     void init() {
-        tvLocInfo = (TextView)findViewById(R.id.locinfo);
+        FragmentManager fragmentManager = getFragmentManager();
+        mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.map);
+        map = mapFragment.getMap();
 
-        FragmentManager myFragmentManager = getFragmentManager();
-        MapFragment myMapFragment = (MapFragment)myFragmentManager.findFragmentById(R.id.map);
-        myMap = myMapFragment.getMap();
+        map.setMyLocationEnabled(true);
 
-        myMap.setMyLocationEnabled(true);
+        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
-        myMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        map.setOnMapClickListener(this);
+        map.setOnMapLongClickListener(this);
+        map.setOnMarkerDragListener(this);
 
-        myMap.setOnMapClickListener(this);
-        myMap.setOnMapLongClickListener(this);
-        myMap.setOnMarkerDragListener(this);
+        focusCamera();
+        drawBoundaries();
+    }
 
-        markerClicked = false;
+    public void focusCamera() {
+        // centers camera around OU
+        LatLngBounds OAKLAND = new LatLngBounds(
+                new LatLng(42.67076892499418, -83.21840766817331),
+                new LatLng(42.67571776885421, -83.21245819330215));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(OAKLAND.getCenter(), 15));
+    }
+
+    public void drawBoundaries() {
+        // draws rough lines around EC parking lot
+        PolylineOptions rectOptions = new PolylineOptions()
+                .add(new LatLng(42.6713583, -83.215614482)) // north west
+                .add(new LatLng(42.670903, -83.215614482)) // south west
+                .add(new LatLng(42.670903, -83.21417212)) // south east
+                .add(new LatLng(42.671420707, -83.214113451))  // north east
+                .add(new LatLng(42.6713583, -83.215614482)); // closing at north west
+
+        Polyline polyline = map.addPolyline(rectOptions);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_map, menu);
         return true;
-
     }
 
     @Override
@@ -78,7 +91,6 @@ public class MapPageActivity extends BaseActivity implements OnMapClickListener,
                 LicenseDialog.setTitle("Legal Notices");
                 LicenseDialog.setMessage(LicenseInfo);
                 LicenseDialog.show();
-
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -87,32 +99,26 @@ public class MapPageActivity extends BaseActivity implements OnMapClickListener,
     @Override
     protected void onResume(){
         super.onResume();
-
         int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
-
-        if (resultCode == ConnectionResult.SUCCESS){
+        if (resultCode == ConnectionResult.SUCCESS) {
             Toast.makeText(getApplicationContext(), "isGooglePlayServicesAvailable SUCESS", Toast.LENGTH_LONG).show();
-        }else{
+        } else {
             GooglePlayServicesUtil.getErrorDialog(resultCode, this, RQS_GooglePlayServices);
         }
     }
 
     @Override
-    public void onMapLongClick(LatLng point){
+    public void onMapLongClick(LatLng point) {
         tvLocInfo.setText("New marker added@" + point.toString());
-        myMap.addMarker(new MarkerOptions()
+        map.addMarker(new MarkerOptions()
                 .position(point)
                 .draggable(true));
-
-        markerClicked = false;
     }
 
     @Override
     public void onMapClick(LatLng point) {
         tvLocInfo.setText(point.toString());
-        myMap.animateCamera(CameraUpdateFactory.newLatLng(point));
-
-        markerClicked = false;
+        map.animateCamera(CameraUpdateFactory.newLatLng(point));
     }
 
     public void onMarkerDrag(Marker marker) {
@@ -126,7 +132,5 @@ public class MapPageActivity extends BaseActivity implements OnMapClickListener,
     public void onMarkerDragStart(Marker marker) {
         tvLocInfo.setText("Marker " + marker.getId() + " DragStart");
     }
-
-
 
 }

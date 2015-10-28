e0098d3572d68bf2c9f2c7faeab4142e3b75df0b
Devin <dpkelly@oakland.edu>
2015-10-28 12:44:42 -0400

    Updated to change colors and store in parse, links

--- app/src/main/java/edu/oakland/festinfo/activities/WeatherPageActivity.java
+++ app/src/main/java/edu/oakland/festinfo/activities/WeatherPageActivity.java
@@ -1,23 +1,40 @@
 package edu.oakland.festinfo.activities;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import edu.oakland.festinfo.R;
 /**
  * Created by Devin on 9/30/2015.
  */
-public class WeatherPageActivity extends Fragment {
+public class WeatherPageActivity extends Fragment implements View.OnClickListener{
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.activity_weather_page,container,false);
+        Button link = (Button) v.findViewById(R.id.weather_link_button);
+        link.setOnClickListener(this);
         return v;
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.weather_link_button:
+                Uri uri = Uri.parse("http://www.wunderground.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+        }
+
+    }
+
 }
